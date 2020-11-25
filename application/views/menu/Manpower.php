39852b475fb077101dad30d3e298550785e40b20
Tito Anugerah Maharizky <titoanugerah@student.undip.ac.id>
2020-11-25 22:35:44 +0700

    remove kegiatan

--- application/views/menu/Manpower.php
+++ application/views/menu/Manpower.php
@@ -4,12 +4,12 @@
     <p>Beranda</p>
   </a>
 </li>
-<li class="nav-item " >
+<!-- <li class="nav-item " >
   <a href="<?php echo base_url('event'); ?>">
     <i class="fas fa-sign"></i>
     <p>Kegiatan</p>
   </a>
-</li>
+</li> -->
 <li class="nav-item " >
   <a href="<?php echo base_url('schedule'); ?>">
     <i class="fas fa-clock"></i>
